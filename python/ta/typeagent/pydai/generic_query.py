d0e9edfcffb809e189ae0e90e459010ac3200f36
Guido van Rossum <gvanrossum@microsoft.com>
2025-07-30 14:03:47 -0700

    Add a line for 'Who was that X we talked about' (this is getting ridiculous)

--- python/ta/typeagent/pydai/generic_query.py
+++ python/ta/typeagent/pydai/generic_query.py
@@ -81,6 +81,7 @@ ENTITY NAME RULES:
 ACTION PATTERNS:
 - "What did X say about Y?" → action_verbs: ["say"], actor_entities: [X], target_entities: [Y]
 - "Summarize X's thoughts to Y?" → action_verbs: ["summarize", "explain", "say"], actor_entities: [X], target_entities: [Y]
+- "Who was that X we talked about?" → entity_search_terms: [EntityTerm(name="*", type=["person"])], search_terms: ["X"]
 - "How did X get referenced?" → action_verbs: ["referenced"], actor_entities: "*", target_entities: [X]
 - "Who mentioned X?" → action_verbs: ["mentioned"], actor_entities: "*", target_entities: [X]
 
